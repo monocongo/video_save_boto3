0ee4deb612fdb9f5d3bba7c9315559c7e0845575
James Adams <monocongo@gmail.com>
2019-05-29 16:23:42 -0400

    updates to comments, commented out extraneous code for logging
    #9

--- video_utils/restful_api.py
+++ video_utils/restful_api.py
@@ -16,27 +16,29 @@ logging.basicConfig(level=logging.INFO,
 _logger = logging.getLogger(__name__)
 
 
+# ------------------------------------------------------------------------------
 @app.route('/clip', methods=['GET'])
 def record_and_store_clip():
 
-    message = "Recording video clip with the following parameters:\n" + \
-              f"\tRTSP URL: {request.args.get('rtsp')}\n" + \
-              f"\tStart seconds: {request.args.get('start')}\n" + \
-              f"\tDuration seconds: {request.args.get('duration')}\n" + \
-              f"\tS3 bucket: {request.args.get('bucket')}\n" + \
-              f"\tS3 key prefix: {request.args.get('prefix')}\n"
-
-    _logger.info(message)
+    # message = "Recording video clip with the following parameters:\n" + \
+    #           f"\tRTSP URL: {request.args.get('rtsp')}\n" + \
+    #           f"\tStart seconds: {request.args.get('start')}\n" + \
+    #           f"\tDuration seconds: {request.args.get('duration')}\n" + \
+    #           f"\tS3 bucket: {request.args.get('bucket')}\n" + \
+    #           f"\tS3 key prefix: {request.args.get('prefix')}\n"
+    #
+    # _logger.info(message)
 
     s3_key = collect_and_store(request.args.get('rtsp'),
-                      int(request.args.get('start')),
-                      int(request.args.get('duration')),
-                      request.args.get('bucket'),
-                      request.args.get('prefix'))
+                               int(request.args.get('start')),
+                               int(request.args.get('duration')),
+                               request.args.get('bucket'),
+                               request.args.get('prefix'))
 
     return s3_key
 
 
+# ------------------------------------------------------------------------------
 @app.route('/hello', methods=['GET'])
 def hello():
 
