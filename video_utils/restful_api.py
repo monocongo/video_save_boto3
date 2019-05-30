737b9423b1a36b6ca3419dc73daa5be9c1395c92
James Adams <monocongo@gmail.com>
2019-05-30 17:32:10 -0400

    added a port CLI argument, removed logging
    #13

--- video_utils/restful_api.py
+++ video_utils/restful_api.py
@@ -1,3 +1,4 @@
+import argparse
 import logging
 
 from flask import Flask, request
@@ -9,25 +10,8 @@ app = Flask(__name__)
 
 
 # ------------------------------------------------------------------------------
-# set up a basic, global logger object which will write to the console
-logging.basicConfig(level=logging.INFO,
-                    format="%(asctime)s %(levelname)s  %(message)s",
-                    datefmt="%Y-%m-%d  %H:%M:%S")
-_logger = logging.getLogger(__name__)
-
-
-# ------------------------------------------------------------------------------
 @app.route('/clip', methods=['GET'])
 def record_and_store_clip():
-
-    # message = "Recording video clip with the following parameters:\n" + \
-    #           f"\tRTSP URL: {request.args.get('rtsp')}\n" + \
-    #           f"\tStart seconds: {request.args.get('start')}\n" + \
-    #           f"\tDuration seconds: {request.args.get('duration')}\n" + \
-    #           f"\tS3 bucket: {request.args.get('bucket')}\n" + \
-    #           f"\tS3 key prefix: {request.args.get('prefix')}\n"
-    #
-    # _logger.info(message)
 
     s3_key = collect_and_store(request.args.get('rtsp'),
                                int(request.args.get('start')),
@@ -47,4 +31,13 @@ def hello():
 
 # ------------------------------------------------------------------------------
 if __name__ == '__main__':
-    app.run(debug=True)
+
+    # parse the command line arguments
+    args_parser = argparse.ArgumentParser()
+    args_parser.add_argument("--port",
+                             type=int,
+                             default=5000,
+                             help="port number to run on")
+    args = vars(args_parser.parse_args())
+
+    app.run(host="0.0.0.0", port=args["port"], debug=False)
